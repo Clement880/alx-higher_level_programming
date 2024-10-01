5a9d95d8889cb96bdd142d66043828a8ec0dd016
Clement880 <clementlethata@gmail.com>
2024-10-01 14:10:10 +0000

    0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class named Square.
+"""
+
+class Square:
+    """
+    An empty class that defines a square.
+
+    Attributes:
+        None
+    """
+    pass
